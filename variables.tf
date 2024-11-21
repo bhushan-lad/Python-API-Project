618bfa3761d542a4410b613335da809ad3a29a3e
bhushan-lad <65479949+bhushan-lad@users.noreply.github.com>
2024-11-21 14:50:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key"{
+	default = "<your_key>"
+}
+
+variable "secret_key"{
+	default = "<your_secret_key>"
+}
+
+variable "vpc_id"{
+	default= "<your_vpc_id>"
+}
+
+variable "ami_id"{
+	default = "<ami_id>"
+}
